b465aa613bbe50d7ea1e26f1f8c9c4b48a5a16e4
dylandhw <dylanwilkins2308@gmail.com>
2025-08-12 15:35:15 -0400

    write function to save the blockchain to local storage

--- internal/storage/storage.go
+++ internal/storage/storage.go
@@ -19,6 +19,16 @@ import (
 	"errors"
 )
 
-func SaveBlockChain(filename string, bc *Blockchain) error {}
+func SaveBlockChain(filename string, bc *Blockchain) error {
+	// marshal bc to json
+	JSON_bc, err := json.Marshal(bc)
+	if err != nil {return err}
+
+	// write json bytes to the file
+	err = os.WriteFile(filename, JSON_bc, 0644) 
+	if err != nil {return err}
+
+	return nil // success 
+}
 
 func LoadBlockchain(filename string, bc *Blockchain) error {}
